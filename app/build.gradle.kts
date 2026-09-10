f2d5b84414fb5189dbd9bd2c30cc347a03dc23e7
jabloorock <rorkeporke@gmail.com>
2026-09-10 18:54:29 +0500

    minSdk to 30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,7 @@ android {
     }
     defaultConfig {
         applicationId = "com.ghostlock.app"
-        minSdk = 34
+        minSdk = 30
         targetSdk = 37
         versionCode = gitVersionCode
         versionName = appVersionName
